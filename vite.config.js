5e298928c5f39bd0cb3a6a5808372b19d7367ca4
cyberHawk UG <mcgz0783@gmail.com>
2026-04-24 22:45:07 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 4173,
+  },
+  // Allow env variables prefixed with VITE_ to be used in the app
+  envPrefix: 'VITE_',
+})
